78d561565cebb37eced3346d03d4197912f6b968
Ganesha Danu <ganeshadanu1@gmail.com>
2019-03-27 23:14:19 +0700

    add instruction enum

--- src/gestalt/cpu.rs
+++ src/gestalt/cpu.rs
@@ -1,10 +1,40 @@
+mod instruction;
+
+use instruction::Instruction;
+
 pub struct CPU {
- 
+    cycle: u8,
+    register: Register
 }
 
 impl CPU {
     pub fn new() -> CPU {
-        CPU {}
+        CPU {
+            cycle: 0,
+            register: Register::new()
+        }
+    }
+}
+
+pub struct Register {
+    program_counter: u16,
+    stack_pointer: u8,
+    accumulator: u8,
+    x: u8,
+    y: u8,
+    status: u8
+}
+
+impl Register {
+    pub fn new() -> Register {
+        Register {
+            program_counter: 0,
+            stack_pointer: 0,
+            accumulator: 0,
+            x: 0,
+            y: 0,
+            status: 0
+        }
     }
 }
 
---
+++ src/gestalt/cpu/instruction.rs
@@ -0,0 +1,80 @@
+pub enum Instruction {
+    // load and store operation
+    LDA,
+    LDX,
+    LDY,
+    STA,
+    STX,
+    STY,
+
+    // register transfer
+    TAX,
+    TAY,
+    TXA,
+    TYA,
+
+    // stack operation
+    TSX,
+    TXY,
+    PHA,
+    PHP,
+    PLA,
+    PLP,
+
+    // logical operation
+    AND,
+    EOR,
+    ORA,
+    BIT,
+
+    // arithmetic operation
+    ADC,
+    SBC,
+    CMP,
+    CPX,
+    CPY,
+
+    // inc and dec operation
+    INC,
+    INX,
+    INY,
+    DEC,
+    DEX,
+    DEY,
+
+    // shifts operation
+    ASL,
+    LSR,
+    ROL,
+    ROR,
+
+    // jump operation
+    JMP,
+    JSR,
+    RTS,
+
+    // branch operation
+    BCC,
+    BCS,
+    BEQ,
+    BMI,
+    BNE,
+    BPL,
+    BVC,
+    BVS,
+
+    // status flag changes
+    CLC,
+    CLD,
+    CLI,
+    CLV,
+    SEC,
+    SED,
+    SEI,
+
+    // system function
+    BRK,
+    NOP,
+    RTI
+}
+
